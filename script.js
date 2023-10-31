9f3add5163042ff968a60295e8b69d5fc52762bd
Byte Me <chemerilcollins@gmail.com>
2023-10-31 14:25:23 +0100

    drag but not good

--- script.js
+++ script.js
@@ -250,13 +250,12 @@ function createElement(arr) {
 createElement(shape);
 console.log(randomElement.shape);
 
-
-
-
 function rotateShape() {
   const shape = randomElement.shape;
   const size = shape.length;
-  const rotatedShape = new Array(size).fill(0).map(() => new Array(size).fill(0));
+  const rotatedShape = new Array(size)
+    .fill(0)
+    .map(() => new Array(size).fill(0));
 
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
@@ -266,18 +265,17 @@ function rotateShape() {
   randomElement.shape = rotatedShape;
 }
 
-function flipShape(){
+function flipShape() {
   let shape = randomElement.shape;
   let flippedShaped = shape.map((row) => row.reverse());
   randomElement.shape = flippedShaped;
 }
 
-function clearCells(){
+function clearCells() {
   cells.forEach((cell) => {
     cell.style.backgroundImage = "";
   });
 }
-
 
 let rotateBtn = document.querySelector(".rotate");
 let flipBtn = document.querySelector(".flip");
@@ -287,24 +285,66 @@ rotateBtn.addEventListener("click", () => {
   createElement(randomElement.shape);
 });
 
-
 flipBtn.addEventListener("click", () => {
   clearCells();
   flipShape();
   createElement(randomElement.shape);
 });
 
-
-
-
-
-
-
-
-
-
+// !RANDOM DATA. WILL REMOVE
 // *Points
 const points = document.querySelectorAll(".points");
 points.forEach((element) => {
   element.innerHTML = Math.floor(Math.random() * 10);
+});
+
+
+
+// *Dragging and dropping shapes on the map
+
+let mapCells = document.querySelectorAll(".cell");
+let shapeContainer = document.querySelector(".randomShape");
+
+// Assuming shapeContainer is the container of the shape
+shapeContainer.setAttribute("draggable", "true");
+
+shapeContainer.addEventListener("dragstart", (event) => {
+  event.dataTransfer.setData("text/plain", JSON.stringify(randomElement));
+});
+
+mapCells.forEach((cell) => {
+  cell.addEventListener("dragover", (event) => {
+    event.preventDefault(); // This is necessary to allow a drop operation
+  });
+
+  cell.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const randomElementData = JSON.parse(
+      event.dataTransfer.getData("text/plain")
+    );
+    const shape = randomElementData.shape;
+    const cellSize = 50; // Replace with the actual size of a cell
+    const dropRow = Math.floor(event.clientY / cellSize);
+    const dropCol = Math.floor(event.clientX / cellSize);
+
+    // Create the shape on the map based on the shape property of the randomElement object
+    for (let row = 0; row < shape.length; row++) {
+      for (let col = 0; col < shape[row].length; col++) {
+        if (shape[row][col] === 1) {
+          const mapRow = dropRow + row;
+          const mapCol = dropCol + col;
+    
+          if (randomElementData.type === "water") {
+            mapCells[mapRow * 3 + mapCol].style.backgroundImage = `url("./assets/tiles/water_tile.png")`;
+          } else if (randomElementData.type === "forest") {
+            mapCells[mapRow * 3 + mapCol].style.backgroundImage = `url("./assets/tiles/forest_tile.png")`;
+          } else if (randomElementData.type === "farm") {
+            mapCells[mapRow * 3 + mapCol].style.backgroundImage = `url("./assets/tiles/plains_tile.png")`;
+          } else if (randomElementData.type === "town") {
+            mapCells[mapRow * 3 + mapCol].style.backgroundImage = `url("./assets/tiles/village_tile.png")`;
+          }
+        }
+      }
+    }
+  });
 });
